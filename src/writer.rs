4e54cc341809a0ed0146f33c7479a766c1290843
Yuuna Paulina Jahn <yuuna@yumix.org>
2025-02-16 16:11:40 +0100

    Implement WebSocketWriter::send_binary()

--- src/writer.rs
+++ src/writer.rs
@@ -1,15 +1,20 @@
 use std::net::TcpStream;
 
 use bevy::prelude::*;
-use websocket::{sync::Writer, OwnedMessage, WebSocketError};
+pub use websocket::WebSocketError;
+use websocket::{sync::Writer, OwnedMessage};
 
 #[derive(Resource)]
 pub struct WebSocketWriter<'s> {
     pub(crate) sender: &'s mut Writer<TcpStream>,
 }
 impl WebSocketWriter<'_> {
-    pub fn send_message<M: ToString>(&mut self, message: M) -> Result<(), WebSocketError> {
+    pub fn send_message<M: ToString>(&mut self, data: M) -> Result<(), WebSocketError> {
         self.sender
-            .send_message(&OwnedMessage::Text(message.to_string()))
+            .send_message(&OwnedMessage::Text(data.to_string()))
+    }
+
+    pub fn send_binary(&mut self, data: Vec<u8>) -> Result<(), WebSocketError> {
+        self.sender.send_message(&OwnedMessage::Binary(data))
     }
 }
